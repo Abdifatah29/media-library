815a3ec42090f688f8f707b40f343f9a4abc6a4a
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-23 00:18:17 +0200

    used a variable  to add active states to nav

--- catalog.php
+++ catalog.php
@@ -1,13 +1,18 @@
 <?php 
 $pageTitle = "Full Catalog";
+//for default
+$section = null;
 
 if (isset($_GET["cat"])) {
  if ($_GET["cat"] == "books") {
         $pageTitle = "books";
+        $section = "books";
     } else if ($_GET["cat"] == "movies") {
         $pageTitle = "movies";
+        $section = "movies";
     } else if ($_GET["cat"] == "music") {
         $pageTitle = "music";
+        $section = "music";
     } 
 }
 include("inc/header.php");?> <!-- Including header.php-->
