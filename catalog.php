4391a6342e84c0655dd80abc6b425095d020e2dc
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-22 23:41:45 +0200

    used _GET variable to set diff page titles

--- catalog.php
+++ catalog.php
@@ -1,8 +1,18 @@
 <?php 
 $pageTitle = "Full Catalog";
+
+if (isset($_GET["cat"])) {
+ if ($_GET["cat"] == "books") {
+        $pageTitle = "books";
+    } else if ($_GET["cat"] == "movies") {
+        $pageTitle = "movies";
+    } else if ($_GET["cat"] == "music") {
+        $pageTitle = "music";
+    } 
+}
 include("inc/header.php");?> <!-- Including header.php-->
 <div class="section page">
-    <h1>Full Catalog</h1>
+    <h1><?php echo $pageTitle ?></h1>
 </div>
 
 <?php include("inc/footer.php")?> <!-- Including footer.php-->
